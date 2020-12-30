b80fbe44f8950ebb13c68b4d30910502c3d45a56
sebkoo <seb.m.koo@gmail.com>
2020-12-30 13:45:29 -0500

    Init Comit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Sebastian B. Koo</Text>
+      <Text>Falls Church, VA</Text>
+      <Text>BS in Computer Science</Text>
+      <Image source={require('./assets/profile.jpg')}/>
       <StatusBar style="auto" />
     </View>
   );
